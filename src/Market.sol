f3cd671c676b2f53321f42d35af923c651715d4a
Maksim Tsepkov <max@dappvibe.com>
2024-06-19 19:15:09 +0800

    Market.createOffer() draft

---
+++ src/Market.sol
@@ -0,0 +1,68 @@
+pragma solidity ^0.8.0;
+
+import "./Deal.sol";
+import "../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import {ERC721} from "../lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import {IMarket} from "./interfaces/IMarket.sol";
+import {Fiat} from "./enums/fiats.sol";
+import {Country} from "./enums/countries.sol";
+import {PaymentMethodOracle} from "./oracles/PaymentMethodOracle.sol";
+
+/**
+ * @title Market
+ * @dev Emits events to securely broadcast ads, reputation.
+ *   Tracks txs and feedback to build reputation.
+ */
+contract Market is
+    IMarket
+{
+    mapping(uint24 => Offer)    public offers;
+    uint24 private _nextOfferId;
+
+    mapping(uint32 => Deal)     public deal;
+    uint32 private _nextDealId;
+
+    // feedback is in blockchain logs?
+    // transactions is in blockchain logs?
+
+    // TODO multiple addresses link rep (in a way protected from DDoS clients when there are too many linked account to fetch logs for)
+
+    constructor(){
+
+    }
+
+    struct OfferCreateParams {
+        bool isSell;
+        address crypto; // ERC20
+        Fiat fiat;
+        uint price;
+        uint min;
+        uint max;
+        bytes16 deliveryMethod;
+        uint16 paymentTimelimit; // protection from stalled deals. after expiry seller can request refund and buyer still gets failed tx recorded
+        string terms; // FIXME can it be another contract deployed by advertiser?
+    }
+
+    function offerCreate(OfferCreateParams calldata _params) external returns(uint)
+    {
+        offers[_nextOfferId] = Offer({
+            owner: msg.sender,
+            isSell: _params.isSell,
+            crypto: _params.crypto,
+            fiat: _params.fiat,
+            priceRatio: _params.price,
+            fiatMin: _params.min,
+            fiatMax: _params.max,
+            deliveryMethod: _params.deliveryMethod,
+            paymentTimelimit: _params.paymentTimelimit,
+            terms: _params.terms,
+            kycRequired: false,
+            active: true
+        });
+
+        emit OfferCreated(_params.isSell, _params.crypto, _params.fiat, _nextOfferId, offers[_nextOfferId]);
+
+        _nextOfferId++;
+        return _nextOfferId - 1;
+    }
+}
