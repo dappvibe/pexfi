01d241446c28a81f0aa5779155ae519c5e68737e
Maksim <max@dappvibe.com>
2024-06-19 21:34:09 +0800

    Full test for Market

--- test/Market.js
+++ test/Market.js
@@ -1,8 +1,41 @@
 const {expect} = require("chai");
+const {ethers} = require("hardhat");
+const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
+const { loadFixture } = require("@nomicfoundation/hardhat-toolbox/network-helpers");
+
+const WETH = '0xfff9976782d46cc05630d1f6ebab18b2324d6b14';
 
 describe("Market", function() {
-    it("Deployment does nothing", async function() {
-        const [owner] = await ethers.getSigners();
+    async function deploy() {
+        const [owner, seller, buyer, mediator]
+            = await ethers.getSigners();
         const market = await ethers.deployContract("Market");
+        await market.waitForDeployment();
+        return { market, owner, seller, buyer, mediator };
+    }
+
+    it("Deployment does nothing", async function() {
+        const market = await loadFixture(deploy);
+    });
+
+    it('Creates offer', async function() {
+        const { market } = await loadFixture(deploy);
+        const params = {
+            isSell: true,
+            crypto: WETH,
+            fiat: 43, // EUR
+            price: 100,
+            min: 1000,
+            max: 5000,
+            deliveryMethod: 'bank',
+            paymentTimeLimit: 60,
+            terms: 'No KYC'
+        };
+
+        const response = await market.offerCreate(params).then((tx) => tx.wait());
+        await expect(response)
+            .to.emit(market, 'OfferCreated')
+            // bugged plugin changes WETH address case
+            .withArgs(true, '0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14', 43, 0, anyValue);
     });
 });
