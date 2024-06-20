f800611e04461f082e29e68cc67fc90a34ad61bb
Maksim Tsepkov <max@dappvibe.com>
2024-06-20 14:21:14 +0800

    validate method on offerCreate()
    this commit also refactor tests to deploy once which preserves states between tests and MUCH faster

--- src/Market/OfferManager.sol
+++ src/Market/OfferManager.sol
@@ -10,8 +10,8 @@ contract OfferManager is IOfferManager, OwnableUpgradeable
     mapping(uint24 => Offer)    public offers;
     uint24 private _nextOfferId;
 
-    mapping(uint16 => Method) public deliveryMethods;
-    uint16 private _nextDeliveryMethodId;
+    mapping(uint16 => Method) public methods;
+    uint16 private _nextMethodId;
 
     function offerCreate(OfferCreateParams calldata _params) external returns(uint)
     {
@@ -21,6 +21,9 @@ contract OfferManager is IOfferManager, OwnableUpgradeable
         require (_params.max > 0, "max must be greater than 0");
         require (_params.paymentTimeLimit >= 15, "paymentTimeLimit must be greater or equal to 15 minutes");
 
+        Method memory method = methods[_params.method];
+        require(bytes(method.name).length > 0, "method is not available");
+
         offers[_nextOfferId] = Offer({
             owner: msg.sender,
             isSell: _params.isSell,
@@ -29,7 +32,7 @@ contract OfferManager is IOfferManager, OwnableUpgradeable
             priceRatio: _params.price,
             fiatMin: _params.min,
             fiatMax: _params.max,
-            deliveryMethod: _params.deliveryMethod,
+            method: method,
             paymentTimelimit: _params.paymentTimeLimit,
             terms: _params.terms,
             kycRequired: false,
@@ -44,21 +47,24 @@ contract OfferManager is IOfferManager, OwnableUpgradeable
 
     function methodAdd(string calldata _name, MethodGroup _group, Country _country) external onlyOwner returns(uint16)
     {
-        deliveryMethods[_nextDeliveryMethodId] = Method({
+        methods[_nextMethodId] = Method({
             name: _name,
             group: _group,
             country: _country
         });
 
-        emit MethodAdded(_nextDeliveryMethodId, _country, deliveryMethods[_nextDeliveryMethodId]);
+        emit MethodAdded(_nextMethodId, _country, methods[_nextMethodId]);
 
-        _nextDeliveryMethodId++;
-        return _nextDeliveryMethodId - 1;
+        _nextMethodId++;
+        return _nextMethodId - 1;
     }
 
     function methodRemove(uint16 _methodId) external onlyOwner
     {
-        delete deliveryMethods[_methodId];
+        require(_methodId > 0, "Cannot remove default value");
+        require(_methodId < _nextMethodId, "method does not exist");
+
+        delete methods[_methodId];
         emit MethodRemoved(_methodId);
     }
 }
