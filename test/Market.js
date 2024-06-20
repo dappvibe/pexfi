97e5459b5859919cf8732b5ae308f8bfe635d483
Maksim Tsepkov <max@dappvibe.com>
2024-06-20 13:32:39 +0800

    payment method add/remove

--- test/Market.js
+++ test/Market.js
@@ -23,6 +23,24 @@ describe("Market", function() {
         expect(newMarket.owner()).to.eventually.eq(owner.address);
     });
 
+    it('Add delivery method', async function() {
+        const { market } = await loadFixture(deploy);
+        const methods = [
+            {name: 'Zelle', group: 3, country: 188},
+            {name: 'SEPA',  group: 3, country: 1},
+            {name: 'Monero', group: 1, country: 0},
+            {name: 'Cash To ATM',  group: 2, country: 0},
+        ];
+        for (const method of methods) {
+            let receipt = await market.methodAdd(method.name, method.group, method.country).then((tx) => tx.wait());
+            await expect(receipt).to.emit(market, 'MethodAdded');
+            var methodId = receipt.logs[0].topics[1];
+        }
+
+        const receipt = await market.methodRemove(methodId).then((tx) => tx.wait());
+        await expect(receipt).to.emit(market, 'MethodRemoved');
+    });
+
     it('Create offer', async function() {
         const { market } = await loadFixture(deploy);
         const params = {
@@ -72,6 +90,57 @@ describe("Market", function() {
             min: 1000,
             max: 5000,
             deliveryMethod: 'bank',
+            paymentTimeLimit: 60,
+            terms: 'No KYC'
+        };
+
+        await expect(market.offerCreate(params)).to.be.reverted;
+    });
+
+    it ('Create offer with invalid min', async function() {
+        const {market} = await loadFixture(deploy);
+        const params = {
+            isSell: true,
+            crypto: WETH,
+            fiat: 43,
+            price: 100,
+            min: 0,
+            max: 5000,
+            deliveryMethod: 'bank',
+            paymentTimeLimit: 60,
+            terms: 'No KYC'
+        };
+
+        await expect(market.offerCreate(params)).to.be.reverted;
+    });
+
+    it('Create offer with invalid max', async function() {
+        const {market} = await loadFixture(deploy);
+        const params = {
+            isSell: true,
+            crypto: WETH,
+            fiat: 43,
+            price: 100,
+            min: 1000,
+            max: 0,
+            deliveryMethod: 'bank',
+            paymentTimeLimit: 60,
+            terms: 'No KYC'
+        };
+
+        await expect(market.offerCreate(params)).to.be.reverted;
+    });
+
+    it('Create offer with invalid delivery method', async function() {
+        const {market} = await loadFixture(deploy);
+        const params = {
+            isSell: true,
+            crypto: WETH,
+            fiat: 43,
+            price: 100,
+            min: 1000,
+            max: 5000,
+            deliveryMethod: 'invalid',
             paymentTimeLimit: 60,
             terms: 'No KYC'
         };
