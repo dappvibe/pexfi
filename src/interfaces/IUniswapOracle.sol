32e1662a5c62731cd97028d5bae698112977a4a0
Maksim Tsepkov <max@dappvibe.com>
2024-06-21 18:04:32 +0800

    PriceOracle draft structure

--- src/interfaces/IUniswapOracle.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
-
-interface IUniswapOracle
-{
-    function getPrice(address token0, address token1, uint24 fee, uint32 twapPeriod)
-    external view returns
-    (uint128 price, uint256 lastUpdateTimestamp);
-}
