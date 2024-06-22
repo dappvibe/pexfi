336877656bf25b88bc349c743294f845fe805b60
Maksim Tsepkov <max@dappvibe.com>
2024-06-23 02:05:05 +0800

    view functions on top

--- src/Market.sol
+++ src/Market.sol
@@ -56,6 +56,19 @@ contract Market is
     }
     function _authorizeUpgrade(address) internal onlyOwner override {}
 
+    function tokens() external view returns (Token[] memory) {
+        uint256 length = _tokens.length();
+        Token[] memory result = new Token[](length);
+        for (uint256 i = 0; i < length; i++) {
+            result[i] = token[_tokens.at(i)];
+        }
+        return result;
+    }
+
+    function fiats() external view returns (bytes32[] memory) {
+        return _fiats.values();
+    }
+
     function addToken(Token calldata token_) external onlyOwner {
         if (_tokens.add(token_.target)) {
             token[token_.target] = token_;
@@ -66,23 +79,12 @@ contract Market is
             delete token[token_];
         }
     }
-    function tokens() external view returns (Token[] memory) {
-        uint256 length = _tokens.length();
-        Token[] memory result = new Token[](length);
-        for (uint256 i = 0; i < length; i++) {
-            result[i] = token[_tokens.at(i)];
-        }
-        return result;
-    }
 
     function addFiat(bytes32 fiat_) external onlyOwner {
         _fiats.add(fiat_);
     }
     function removeFiat(bytes32 fiat_) external onlyOwner {
         _fiats.remove(fiat_);
-    }
-    function fiats() external view returns (bytes32[] memory) {
-        return _fiats.values();
     }
 
     function setRepToken(address _repToken) external onlyOwner {
