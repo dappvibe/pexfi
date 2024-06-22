ac68f5581252a6c3efdf3486b9ff35226c4d03d4
Maksim Tsepkov <max@dappvibe.com>
2024-06-22 17:12:24 +0800

    repToken register()

--- test/RepToken.js
+++ test/RepToken.js
@@ -9,22 +9,35 @@ const deployRepToken = async function deployRepToken() {
 module.exports = { deployRepToken };
 
 describe("RepToken", function(){
-    let repToken, deployer, coolhacker;
+    let repToken, deployer, seller, buyer, coolhacker;
 
     before(async function() {
-        [deployer, coolhacker] = await ethers.getSigners();
+        [deployer, seller, buyer, coolhacker] = await ethers.getSigners();
         repToken = await deployRepToken();
     });
 
-    it("is upgradable", async function() {
-        const RepTokenFactory = await ethers.getContractFactory("RepToken");
-        const newRepToken = await upgrades.upgradeProxy(await repToken.getAddress(), RepTokenFactory);
+    describe('deploy', function() {
+        it("is upgradable", async function() {
+            const RepTokenFactory = await ethers.getContractFactory("RepToken");
+            const newRepToken = await upgrades.upgradeProxy(await repToken.getAddress(), RepTokenFactory);
+        });
+
+        it('deployer is admin', async function() {
+            expect(repToken.hasRole('DEFAULT_ADMIN_ROLE', deployer.address))
+                .to.eventually.true;
+            expect(repToken.hasRole('DEFAULT_ADMIN_ROLE', coolhacker.address))
+                .to.eventually.false;
+        });
     });
 
-    it('deployer is admin', async function() {
-        expect(repToken.hasRole('DEFAULT_ADMIN_ROLE', deployer.address))
-            .to.eventually.true;
-        expect(repToken.hasRole('DEFAULT_ADMIN_ROLE', coolhacker.address))
-            .to.eventually.false;
+    describe('register an address', function() {
+        it ('anyone can mint new profile', async function() {
+            await repToken.connect(seller).register();
+            expect(repToken.balanceOf(seller.address)).to.eventually.eq(1);
+            await repToken.connect(buyer).register();
+            expect(repToken.balanceOf(seller.address)).to.eventually.eq(1);
+            await repToken.connect(coolhacker).register();
+            expect(repToken.balanceOf(seller.address)).to.eventually.eq(1);
+        });
     });
 });
