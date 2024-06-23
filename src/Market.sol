acf903fda186f56ac772080fec6d7d37aeb58294
Maksim <max@dappvibe.com>
2024-06-24 03:07:49 +0800

    require solidity ^0.8.20

--- src/Market.sol
+++ src/Market.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
 import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
 import {FullMath} from "../lib/v4-core/src/libraries/FullMath.sol";
