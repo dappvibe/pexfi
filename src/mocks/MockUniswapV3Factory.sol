d99b7680e184127740155bd9963e437ba15a588f
Maksim Tsepkov <max@dappvibe.com>
2024-06-24 02:58:53 +0800

    implement uniswap pool request directly

--- src/mocks/MockUniswapV3Factory.sol
+++ src/mocks/MockUniswapV3Factory.sol
@@ -2,7 +2,27 @@
 pragma solidity ^0.8.0;
 
 contract MockUniswapV3Factory {
-    constructor(){
+    function getPool(
+        address tokenA,
+        address tokenB,
+        uint24 fee
+    ) external view returns (address pool)
+    {
+        return address(this);
+    }
 
+    function observe(uint32[] calldata secondsAgos)
+    external
+    view
+    returns (int56[] memory tickCumulatives, uint160[] memory secondsPerLiquidityCumulativeX128s)
+    {
+        // WETH / USDT pool
+        int56[] memory tickCumulatives = new int56[](2);
+        tickCumulatives[0] = -17595288323282;
+        tickCumulatives[1] = -17595346759502;
+        uint160[] memory secondsPerLiquidityCumulativeX128s = new uint160[](2);
+        secondsPerLiquidityCumulativeX128s[0] = 431388776603627029670683737059;
+        secondsPerLiquidityCumulativeX128s[1] = 431388776750562642834933065513;
+        return (tickCumulatives, secondsPerLiquidityCumulativeX128s);
     }
 }
