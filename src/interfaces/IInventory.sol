c03653b9f9e675582002cf820beb12c4629c792c
Maksim Tsepkov <max@dappvibe.com>
2024-06-26 11:28:54 +0800

    fiats library

--- src/interfaces/IInventory.sol
+++ src/interfaces/IInventory.sol
@@ -4,9 +4,6 @@ pragma solidity ^0.8.0;
 import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
 interface IInventory {
-    event FiatAdded(string indexed symbol, address indexed oracle);
-    event FiatRemoved(string indexed symbol);
-
     function token(string memory symbol_) external view returns (IERC20Metadata);
     function convert(uint amount_, string memory fromFiat_, string memory toToken_, uint denominator) external view returns (uint256);
     function getPrice(string memory token_, string memory fiat_) external view returns (uint256 $result);
