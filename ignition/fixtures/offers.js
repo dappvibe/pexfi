8cfddb3f77772e85f64b4c56a168c4b0cf80bb35
Maksim Tsepkov <max@dappvibe.com>
2024-06-29 03:45:31 +0800

    much more fixtures for pairs

--- ignition/fixtures/offers.js
+++ ignition/fixtures/offers.js
@@ -12,17 +12,17 @@ async function main() {
     await contract.connect(signers[1]);
 
     try {
-        [
-            [true, 'WBTC', 'USD', 'Zelle', 10250, 1000, 5000, ''],
-            [true, 'WBTC', 'THB', 'Zelle', 10400, 100,  1000, ''],
-            [true, 'WETH', 'EUR', 'SEPA',  10250, 1000, 5000, ''],
-            [true, 'USDT', 'USD', 'Zelle', 10250, 1000, 5000, 'arbitrary terms'],
-            [false, 'WBTC', 'USD', 'Zelle', 9800, 1000, 5000, ''],
-            [false, 'WBTC', 'RUB', 'Zelle', 9650, 100,  1000, ''],
-            [false, 'WETH', 'EUR', 'SEPA',  9750, 1000, 5000, ''],
-            [false, 'USDT', 'USD', 'Zelle', 9950, 1000, 5000, ''],
-        ].forEach((offer) => {
-            contract.createOffer([...offer]).then(tx => tx.wait().then(receipt => console.log(receipt)));
+        const tokens = ['WBTC', 'WETH', 'USDT'];
+        const fiats = ['USD', 'EUR', 'THB', 'RUB', 'CNY', 'GBP'];
+
+        tokens.forEach(token => {
+            fiats.forEach(fiat => {
+                let min = 0;
+                for (let i = 0; i < 10; i++){
+                    contract.createOffer([true, token, fiat, 'Zelle', 10000 + Math.floor(Math.random() * 500), min = Math.floor(Math.random() * 1000) + 100, min + 100, '']);
+                    contract.createOffer([false, token, fiat, 'Zelle', 10000 + Math.floor(Math.random() * 500), min = Math.floor(Math.random() * 1000) + 100, min + 100, '']);
+                }
+            });
         });
     } catch (error) {
         console.error(error);
