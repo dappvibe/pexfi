4d100aef8f3fe0064d14738aa521e523f0060e6f
Maksim Tsepkov <max@dappvibe.com>
2024-06-29 03:45:20 +0800

    log error

--- ignition/updateRates.js
+++ ignition/updateRates.js
@@ -14,10 +14,12 @@ fetch('https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currenci
                 let rate = rates[currency.toLowerCase()];
                 if (rate) {
                     ethers.getContractAt('PriceFeed', contracts[id]).then(contract => {
-                        contract.set(Number(rate * Math.pow(10, 8)).toFixed()).then(() => {
-                            console.log(currency + ': ' + rate);
-                        });
-                    });
+                        contract.set(Number(rate * Math.pow(10, 8)).toFixed()).then((tx) => {
+                            tx.wait().then(receipt => {
+                                console.log(currency + ': ' + rate);
+                            });
+                        }).catch(console.error);
+                    }).catch(console.error);
                 }
                 else console.log('No rate for ' + currency);
             }
