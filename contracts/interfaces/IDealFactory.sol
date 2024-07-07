4544eb81a5df0afc6684fc5f5fd56560a11b24fa
Maksim Tsepkov <max@dappvibe.com>
2024-07-07 23:10:32 +0800

    create deals with DealFactory
    this does not reduce fees yet but TWICE lower Market contract which makes possible to merge Inventory back to Market

---
+++ contracts/interfaces/IDealFactory.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+interface IDealFactory
+{
+    function create(
+        address repToken_,
+        uint offerId_,
+        bool isSell,
+        address maker_,
+        address taker_,
+        address mediator_,
+        address token_,
+        uint tokenAmount_,
+        uint fiatAmount_,
+        uint fee_,
+        string memory paymentInstructions_
+    ) external returns (address);
+}
