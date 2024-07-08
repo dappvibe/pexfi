39bf74b154c1f4f601203f56372d7c46efb14c9e
Maksim <max@dappvibe.com>
2024-07-08 10:36:06 +0800

    merge Inventory into Market

--- contracts/interfaces/IMarket.sol
+++ contracts/interfaces/IMarket.sol
@@ -1,10 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
+import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+
 import "../Deal.sol";
 import "../libraries/Offers.sol";
 import "../libraries/Methods.sol";
 import "../interfaces/IRepToken.sol";
+import "../libraries/Fiats.sol";
 
 interface IMarket
 {
@@ -16,4 +19,10 @@ interface IMarket
     */
     function fundDeal() external returns (bool);
     function repToken() external view returns (IRepToken);
+
+    function token(string memory symbol_) external view returns (IERC20Metadata);
+    function fiat(string memory symbol_) external view returns (Fiats.Fiat memory);
+    function method(string memory symbol_) external view returns (Methods.Method memory);
+    function convert(uint amount_, string memory fromFiat_, string memory toToken_, uint denominator) external view returns (uint256);
+    function getPrice(string memory token_, string memory fiat_) external view returns (uint256 $result);
 }
