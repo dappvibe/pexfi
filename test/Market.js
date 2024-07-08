88ab6b83d10f823d2a20a8ec9cad9e07587934ae
Maksim Tsepkov <max@dappvibe.com>
2024-07-08 21:21:27 +0800

    correct log index

--- test/Market.js
+++ test/Market.js
@@ -240,7 +240,7 @@ describe('Users post offers', function()
             OfferFactory = OfferFactory.connect(provider);
             const tx = await OfferFactory.create(params[0], params[1], params[2], params[3], params[4], [params[5], params[6]], params[7]);
             const receipt = await tx.wait();
-            const OfferCreated = Market.interface.parseLog(receipt.logs[1]);
+            const OfferCreated = Market.interface.parseLog(receipt.logs[0]);
             const offer = await ethers.getContractAt('Offer', OfferCreated.args[3]);
             offers.push(offer);
             await expect(await offer.owner()).to.eq(await provider.getAddress());
