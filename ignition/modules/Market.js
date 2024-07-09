a085e4b2f9ee6163ac499e7297791005cb7f4fda
Maksim Tsepkov <max@dappvibe.com>
2024-07-09 19:44:53 +0800

    use ignition for tests deployment

--- ignition/modules/Market.js
+++ ignition/modules/Market.js
@@ -1,86 +1,33 @@
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 const hre = require("hardhat");
-
-const TokensModule = buildModule("Tokens", (m) => {
-    // those are normally exist already in target network and need NOT be deployed
-    const tokens = [];
-    tokens['USDT'] = m.contract("MockERC20", ["USDT", 6], {id: "MockUSDT"}); // base for USD (must present)
-    tokens['WBTC'] = m.contract("MockERC20", ["WBTC", 8], {id: "MockWBTC"});
-    tokens['WETH'] = m.contract("MockERC20", ["WETH", 18], {id: "MockWETH"});
-
-    return { tokens };
-});
-
-const UniswapModule = buildModule("Uniswap", (m) => {
-    const { tokens } = m.useModule(TokensModule);
-
-    // TODO use forked arbitrum mainnet
-    const PoolBTC = m.contract('PoolBTC');
-    const PoolETH = m.contract('PoolETH');
-    const uniswap = m.contract('MockUniswapV3Factory');
-    m.call(uniswap, "setPool", [tokens['WBTC'], PoolBTC], {id: "setPoolBTC"});
-    m.call(uniswap, "setPool", [tokens['WETH'], PoolETH], {id: "setPoolETH"});
-
-    return { uniswap };
-});
-
-const RepModule = buildModule("Rep", (m) => {
-    const RepToken = m.contract('RepToken', []);
-    m.call(RepToken, 'initialize');
-
-    return { RepToken };
-});
-
-const PriceFeedsModule = buildModule("PriceFeeds", (m) => {
-    // TODO provide chainlink price feeds
-    const currencies = require('../currencies.json');
-
-    const fiats = [];
-    currencies.forEach(currency => {
-        //if (currency.chainlink) return; // in production must pass chainlink address
-        fiats.push([currency.code, m.contract(`PriceFeed`, [currency.code], {id: currency.code})]);
-    });
-    // TODO find API to sync prices and upload to contracts
-
-    return { fiats };
-});
-
-const InventoryModule = buildModule("Inventory", (m) => {
-    const { uniswap } = m.useModule(UniswapModule);
-    const { tokens } = m.useModule(TokensModule);
-    const { fiats } = m.useModule(PriceFeedsModule);
-    // TODO real list of methods
-    const methods = [];
-    methods.push({name: 'Zelle', group: 3});
-    methods.push({name: 'SEPA', group: 3});
-
-    const Inventory = m.contract('Inventory', [uniswap]);
-    for (let key in tokens) {
-        m.call(Inventory, 'addTokens', [[tokens[key]], 500], {id: `addToken${key}`});
-    }
-    m.call(Inventory, 'addFiats', [fiats]);
-    // add USD so that it renders in list of fiats in the UI
-    m.call(Inventory, 'addMethods', [methods]);
-
-    return { Inventory };
-});
+const OfferFactoryModule = require("./OfferFactory");
+const DealFactoryModule = require("./DealFactory");
+const RepTokenModule = require("./RepToken");
 
 module.exports = buildModule("Market", (m) =>
 {
-    const { uniswapFactory } = m.useModule(UniswapModule);
-    const { tokens } = m.useModule(TokensModule);
-    const { RepToken } = m.useModule(RepModule);
-    const { fiats } = m.useModule(PriceFeedsModule);
-    const { Inventory } = m.useModule(InventoryModule);
+    const { OfferFactory } = m.useModule(OfferFactoryModule);
+    const { DealFactory }  = m.useModule(DealFactoryModule);
+    const { RepToken } = m.useModule(RepTokenModule);
+    const uniswap = m.getParameter('uniswap'); // factory (or mock) address
 
-    const market = m.contract("Market", []);
-
+    // deploy
+    const impl = m.contract("Market", [], {id: "V0"});
     const proxy = m.contract('ERC1967Proxy', [
-        market,
-        m.encodeFunctionCall(market, 'initialize', [RepToken, Inventory])
+        impl,
+        m.encodeFunctionCall(impl, 'initialize', [OfferFactory, DealFactory, RepToken, uniswap])
     ]);
+    const Market = m.contractAt('Market', proxy);
 
+    // post-deploy data population
+    m.call(Market, 'addTokens', [m.getParameter('addTokens_0'), m.getParameter('addTokens_1')]);
+    m.call(Market, 'addFiats', [m.getParameter('fiats')]);
+    m.call(Market, 'addMethods', [m.getParameter('methods')]);
+
+    // link it all together
+    m.call(OfferFactory, 'initialize', [Market]);
+    m.call(DealFactory, 'initialize', [Market]);
     m.call(RepToken, 'grantRole', [hre.ethers.encodeBytes32String('MARKET_ROLE'), proxy]);
 
-    return { market };
+    return { Market, OfferFactory, DealFactory, RepToken };
 });
