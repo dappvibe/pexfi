10f69b427d69b97d2897d7722da2aa96c6043f99
Maksim Tsepkov <max@dappvibe.com>
2024-07-09 19:44:53 +0800

    use ignition for tests deployment

--- ignition/modules/OfferFactory.js
+++ ignition/modules/OfferFactory.js
@@ -1,8 +1,12 @@
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
 const OfferFactoryModule = buildModule('OfferFactory', m => {
-    const OfferFactory = m.contract('OfferFactory');
-    // TODO set market address with initialize()
+    const impl = m.contract('OfferFactory', [], {id: 'V0'});
+    const proxy = m.contract('ERC1967Proxy', [
+        impl,
+        '0x'    // initialize later
+    ]);
+    const OfferFactory = m.contractAt('OfferFactory', proxy);
 
     return { OfferFactory };
 });
