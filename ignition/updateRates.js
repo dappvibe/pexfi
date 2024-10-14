a9dd071abdc7b1fc1f85f6f188415b06fd760378
Maksim Tsepkov <max@dappvibe.com>
2024-10-14 18:48:42 +0500

    latest updateRates.js as is

--- ignition/updateRates.js
+++ ignition/updateRates.js
@@ -1,29 +1,35 @@
 const currencies = require('./currencies.json');
 const { ethers } = require('hardhat');
+const contracts = require('./deployments/chain-421614/deployed_addresses.json');
 
-const contracts = require('./deployments/chain-31337/deployed_addresses.json');
-
-// https://github.com/fawazahmed0/exchange-api?tab=readme-ov-file
-fetch('https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/usd.json').then(res => {
-    const json = res.json().then(rates => {
-        rates = rates.usd;
+async function updateRates() {
+    try {
+        const response = await fetch('https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/usd.json');
+        const json = await response.json();
+        let rates = json.usd;
         for (let id in contracts) {
             let parts = id.split('#');
             if (parts[0] === 'PriceFeeds') {
                 let currency = parts[1];
                 let rate = rates[currency.toLowerCase()];
                 if (rate) {
-                    ethers.getContractAt('PriceFeed', contracts[id]).then(contract => {
+                    try {
+                        const contract = await ethers.getContractAt('PriceFeed', contracts[id]);
                         let intrate = Math.round((1 / rate) * 10**8);
-                        contract.set(intrate).then((tx) => {
-                            tx.wait().then(receipt => {
-                                console.log(currency + ': ' + rate);
-                            });
-                        }).catch(console.error);
-                    }).catch(console.error);
+                        const tx = await contract.set(intrate);
+                        const receipt = await tx.wait();
+                        console.log(currency + ': ' + rate);
+                    } catch (error) {
+                        console.error(error);
+                    }
+                } else {
+                    console.log('No rate for ' + currency);
                 }
-                else console.log('No rate for ' + currency);
             }
         }
-    });
-});
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+updateRates();
