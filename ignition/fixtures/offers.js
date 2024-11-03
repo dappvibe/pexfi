02752b26b9363212bf0552ce4513293d21ed64e7
Maksim Tsepkov <max@dappvibe.com>
2024-11-03 07:33:02 +0500

    self-executing function and lower limits

--- ignition/fixtures/offers.js
+++ ignition/fixtures/offers.js
@@ -1,6 +1,6 @@
 const { ethers } = require('hardhat');
 
-async function main() {
+(async () => {
     const signers = await ethers.getSigners();
 
     const contracts = require('../deployments/chain-31337/deployed_addresses.json');
@@ -11,21 +11,19 @@ async function main() {
 
     try {
         const tokens = ['WBTC', 'WETH', 'USDT'];
-        const fiats = ['USD', 'EUR', 'THB', 'RUB', 'CNY', 'GBP', 'PHP', 'JPY', 'KRW', 'INR'];
+        const fiats = ['USD', 'EUR'];
 
         tokens.forEach(token => {
             fiats.forEach(fiat => {
                 let min = 0;
                 for (let i = 0; i < 10; i++){
                     contract = contract.connect(signers[Math.floor(Math.random() * signers.length)]);
-                    contract.create(true, token, fiat, 'Zelle', 10000 + Math.floor(Math.random() * 500), [min = Math.floor(Math.random() * 100000) + 100, min + 100], '');
-                    contract.create(false, token, fiat, 'Zelle', 10000 + Math.floor(Math.random() * 500), [min = Math.floor(Math.random() * 100000) + 100, min + 100], '');
+                    contract.create(true, token, fiat, 'Zelle', 10000 + Math.floor(Math.random() * 500), [min = Math.floor(Math.random() * 1000) + 100, min + 10000], '');
+                    contract.create(false, token, fiat, 'Zelle', 10000 + Math.floor(Math.random() * 500), [min = Math.floor(Math.random() * 1000) + 100, min + 10000], '');
                 }
             });
         });
     } catch (error) {
         console.error(error);
     }
-}
-
-main();
+})();
