8a847a08530947085cd2533ba2dfb3215b0e897f
Maksim Tsepkov <max@dappvibe.com>
2024-12-19 14:28:14 +0700

    logs on deploy

--- ignition/mocks.js
+++ ignition/mocks.js
@@ -1,39 +1,42 @@
 const {ethers, ignition} = require("hardhat");
-const MarketModule = require('./modules/Market');
-
-let Uniswap, PriceFeeds = {}, Tokens = {};
+require('./modules/Market');
 
 (async () => {
+    let Uniswap, PriceFeeds = {}, Tokens = {};
+
     const signers = await ethers.getSigners();
-    // do not use the first signer, it is the deployer. so that real contracts have predictable addresses
+    // do not use the first signer, it is the deployer. so that real contracts have predictable addresses after mocks
     signers.shift();
 
+    // create ERC20 tokens and credit to all test signers
     const TokenFactory = await ethers.getContractFactory("MockERC20", signers[0]);
     for (const token of [ ['WBTC', 8], ['WETH', 18], ['USDT', 6] ]) {
         const t = Tokens[token[0]] = await TokenFactory.deploy(...token);
+        process.stderr.write('Deployed '+token[0]+'\n');
         for (const signer of signers) {
             await t.transfer(signer.address, BigInt(100000 * Math.pow(10, token[1])));
         }
     }
 
-    // uniswap pools (mocks)
+    // uniswap pools for mocks
     Uniswap = await ethers.deployContract('MockUniswapV3Factory', signers[0]);
+    process.stderr.write('Deployed Uniswap\n');
     Uniswap.setPool(Tokens['WBTC'], await ethers.deployContract('PoolBTC'));
     Uniswap.setPool(Tokens['WETH'], await ethers.deployContract('PoolETH'));
 
-    // price feeds
+    // price feeds. in production must pass chainlink address, for mocks set current market rate once
     let rates = await fetch('https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/usd.json');
     rates = await rates.json();
     rates = rates.usd;
     const currencies = require('./currencies.json');
     const factory = await ethers.getContractFactory("PriceFeed", signers[0]);
-    // in production must pass chainlink address, for mocks set current market rate once
     for (let currency of currencies) {
         const contract = PriceFeeds[currency.code] = await factory.deploy(currency.code);
         let rate = rates[currency.code.toLowerCase()];
         if (rate) {
             let intrate = Math.round((1 / rate) * 10**8);
             await contract.set(intrate);
+            process.stderr.write(`USD to ${currency.code}: ${rate}\n`);
         }
     }
 
@@ -54,4 +57,5 @@ let Uniswap, PriceFeeds = {}, Tokens = {};
 
     // Use this to deploy Market
     console.log(JSON.stringify({"Market": params}, null, 2));
+    process.stderr.write('Done\n');
 })();
