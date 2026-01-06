d377fecf89ffd28676e033f20a23a9b8302c53ea
Maksim Tsepkov <max@dappvibe.com>
2026-01-06 10:37:25 +0500

    separate providers config from App (which defines routes)

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -1,17 +1,15 @@
-import { render } from '@testing-library/react';
-import App from '@/App';
-// App likely contains Router, so we might not need to wrap it in MemoryRouter,
-// or if it doesn't, we might need to. Usually App is the root.
-// If App invokes BrowserRouter, we can't wrap it in MemoryRouter easily without refactoring.
-// For now, let's assume we can just render it.
+import { render } from '@testing-library/react'
+import { Providers } from '@/Providers'
+import App from '@/App'
 
 describe('App', () => {
   it('renders without crashing', () => {
-    // If App has a Router inside, this might be fine.
-    // If it expects to be inside a Router, we'd need one.
-    // Based on main.tsx usually wrapping App, let's see.
-    // If we crash, we'll fix it.
-    render(<App />);
-    expect(true).toBeTruthy();
-  });
-});
+    // App contains Router, so we test it within Providers to supply contexts
+    render(
+      <Providers>
+        <App />
+      </Providers>
+    )
+    expect(true).toBeTruthy()
+  })
+})
