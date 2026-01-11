edc97a02c5495325ce28ee7f80794bddf58dd8a1
Maksim <max@dappvibe.com>
2026-01-11 19:09:18 +0500

    fix typo

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     baseURL: 'http://localhost:3000',
-    channel: 'chrome', // use isntalled chrome browser
+    channel: 'chrome', // use installed chrome browser
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
