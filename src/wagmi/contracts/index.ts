526c5e9c93996088de781b4dce3d9f0d905545b3
Maksim Tsepkov <max@dappvibe.com>
2026-01-13 09:10:02 +0500

    index file for contracts hooks

---
+++ src/wagmi/contracts/index.ts
@@ -0,0 +1,3 @@
+export { useDeal, DealState, type Deal, type Feedback } from './useDeal'
+export { useOffer, type Offer, type Token } from './useOffer'
+export { useProfile, type Profile } from './useProfile'
