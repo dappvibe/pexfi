14fa59090ef790fd8b2c939fdd61739ecffe9b87
Maksim Tsepkov <max@dappvibe.com>
2026-02-03 17:37:35 +0500

    install hardhat3 and recompile contracts

---
+++ hardhat.config.ts
@@ -0,0 +1,85 @@
+import hardhatToolboxMochaEthersPlugin from '@nomicfoundation/hardhat-toolbox-mocha-ethers'
+import ignoreWarnings from 'hardhat-ignore-warnings';
+import { defineConfig } from 'hardhat/config'
+
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
+export default defineConfig({
+  plugins: [hardhatToolboxMochaEthersPlugin, ignoreWarnings],
+  paths: {
+    sources: './evm/protocol',
+    tests: './tests/evm',
+    cache: './.cache/hardhat',
+    artifacts: './evm/artifacts',
+    ignition: './evm/ignition',
+  },
+  networks: {
+    hardhat: {
+      type: 'edr-simulated',
+    },
+    arbitrum: {
+      type: 'http',
+      url: process.env.ARBITRUM_URL ? process.env.ARBITRUM_URL : 'https://arb-mainnet.g.alchemy.com/v2/demo',
+      accounts: process.env.DEPLOYER_KEY ? [process.env.DEPLOYER_KEY] : [],
+      chainType: 'op',
+    },
+    'arbitrum-sepolia': {
+      type: 'http',
+      url: 'https://sepolia-rollup.arbitrum.io/rpc',
+      accounts: process.env.DEPLOYER_KEY ? [process.env.DEPLOYER_KEY] : [],
+      chainType: 'op',
+    },
+    sepolia: {
+      type: 'http',
+      url: 'https://eth-sepolia.g.alchemy.com/v2/-pIVMYm22LgfrPb32FWlPaKWjXNmH2id',
+      accounts: [process.env.DEPLOYER_SEPOLIA],
+    },
+  },
+
+  // For contracts source verification on local blockscout
+  etherscan: {
+    apiKey: {
+      // Is not required by blockscout. Can be any non-empty string
+      localhost: 'abc',
+      sepolia: process.env.ETHERSCAN_KEY,
+    },
+    customChains: [
+      {
+        network: 'localhost',
+        chainId: 31337,
+        urls: {
+          apiURL: 'http://localhost/api',
+          browserURL: 'http://localhost/',
+        },
+      },
+    ],
+  },
+  solidity: {
+    version: '0.8.26',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+      viaIR: true,
+    },
+  },
+  dependencyCompiler: {
+    paths: ['@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol'],
+  },
+  warnings: {
+    'evm/protocol/mocks/**/*': {
+      default: 'off',
+    },
+  },
+  gasReporter: {
+    enabled: true,
+    coinmarketcap: process.env.GASREPORTER_COINMARKETCAP,
+    L1: 'ethereum',
+    L2: 'arbitrum',
+    L2Etherscan: process.env.GASREPORTER_ETHERSCAN,
+    excludeContracts: ['MockWBTC'],
+  },
+})
