518e7b0f8a667b33bda3d402f99c6bd35f4284c4
Maksim <max@dappvibe.com>
2026-02-03 13:04:27 +0500

    sepolia deployment artifacts

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -30,6 +30,10 @@ module.exports = {
       url: 'https://sepolia-rollup.arbitrum.io/rpc',
       accounts: process.env.DEPLOYER_KEY ? [process.env.DEPLOYER_KEY] : 'remote',
     },
+    sepolia: {
+      url: 'https://eth-sepolia.g.alchemy.com/v2/-pIVMYm22LgfrPb32FWlPaKWjXNmH2id',
+      accounts: [process.env.DEPLOYER_SEPOLIA],
+    },
   },
 
   // For contracts source verification on local blockscout
@@ -37,6 +41,7 @@ module.exports = {
     apiKey: {
       // Is not required by blockscout. Can be any non-empty string
       localhost: 'abc',
+      sepolia: process.env.ETHERSCAN_KEY,
     },
     customChains: [
       {
