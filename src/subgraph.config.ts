bcc5fccd103ddc56f044bd49e1b1fe97788ab51a
Maksim <max@dappvibe.com>
2026-02-09 11:42:25 +0500

    query latest subgraph URL

--- src/subgraph.config.ts
+++ src/subgraph.config.ts
@@ -1,7 +1,7 @@
 import { mainnet, sepolia, hardhat } from 'wagmi/chains'
 
 export const SUBGRAPH_URLS: Record<number, string> = {
-  [mainnet.id]: 'https://api.studio.thegraph.com/query/1741439/pexfi/v0.0.1',
-  [sepolia.id]: 'https://api.studio.thegraph.com/query/1741439/pexfi/v0.0.1',
+  [mainnet.id]: 'https://api.studio.thegraph.com/query/1741439/pexfi/version/latest',
+  [sepolia.id]: 'https://api.studio.thegraph.com/query/1741439/pexfi/version/latest',
   [hardhat.id]: 'http://localhost:8000/subgraphs/name/sov',
 }
