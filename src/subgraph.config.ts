ef14c44fd988e2ac2f55e41d440afd6b51a70f20
Maksim <max@dappvibe.com>
2026-02-11 19:21:27 +0500

    use in-house subgraph query url for sepolia in frontend

--- src/subgraph.config.ts
+++ src/subgraph.config.ts
@@ -2,6 +2,6 @@ import { mainnet, sepolia, hardhat } from 'wagmi/chains'
 
 export const SUBGRAPH_URLS: Record<number, string> = {
   [mainnet.id]: 'https://api.studio.thegraph.com/query/1741439/pexfi/version/latest',
-  [sepolia.id]: 'https://api.studio.thegraph.com/query/1741439/pexfi/version/latest',
+  [sepolia.id]: 'https://graph.pexfi.com/subgraphs/name/sepolia',
   [hardhat.id]: 'http://localhost:8000/subgraphs/name/sov',
 }
