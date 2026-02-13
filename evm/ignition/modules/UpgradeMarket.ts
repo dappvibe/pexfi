47002f2168e18d893cb5a179ad2f21766c7a7060
Maksim Tsepkov <max@dappvibe.com>
2026-02-14 03:35:04 +0500

    ignition: to upgrade market existing address must be in parameters

--- evm/ignition/modules/UpgradeMarket.ts
+++ evm/ignition/modules/UpgradeMarket.ts
@@ -1,13 +1,11 @@
 import { buildModule } from '@nomicfoundation/hardhat-ignition/modules'
-import MarketModule from './Market.ts'
 
 export default buildModule('UpgradeMarket', (m) => {
-  const { Market } = m.useModule(MarketModule)
-
   // Deploy the new implementation
   const newImpl = m.contract('Market', [], { id: 'MarketImplV2' })
 
   // Upgrade the proxy to the new implementation
+  const Market = m.contractAt('Market', m.getParameter('marketAddress'))
   m.call(Market, 'upgradeToAndCall', [newImpl, '0x'])
 
   return { Market }
