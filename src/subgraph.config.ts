d3be8c8b679f8e66bd6645a90143ed41662c02cc
Maksim <max@dappvibe.com>
2026-02-16 23:58:36 +0500

    subgraph: configure local dev network

--- src/subgraph.config.ts
+++ src/subgraph.config.ts
@@ -3,5 +3,5 @@ import { mainnet, sepolia, hardhat } from 'wagmi/chains'
 export const SUBGRAPH_URLS: Record<number, string> = {
   [mainnet.id]: 'https://api.studio.thegraph.com/query/1741439/pexfi/version/latest',
   [sepolia.id]: 'https://graph.pexfi.com/subgraphs/name/sepolia',
-  [hardhat.id]: 'http://localhost:8000/subgraphs/name/sov',
+  [hardhat.id]: 'http://localhost:8000/subgraphs/name/hardhat',
 }
