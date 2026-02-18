ac5cc7f600fcb1cd15e625e2a8bd9ab0e8a1e719
Maksim Tsepkov <max@dappvibe.com>
2026-02-19 01:41:47 +0500

    remove voting power from PexfiToken
    so that it simplifies and reduces gas. DAO decisions are not in the plan

--- evm/protocol/PexfiVault.sol
+++ evm/protocol/PexfiVault.sol
@@ -4,37 +4,17 @@ pragma solidity 0.8.26;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ERC4626} from "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
 import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
-import {ERC20Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
-import {Nonces} from "@openzeppelin/contracts/utils/Nonces.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "./PexfiToken.sol";
 
-contract PexfiVault is ERC4626, ERC20Permit, ERC20Votes, Ownable {
+contract PexfiVault is ERC4626, ERC20Permit, Ownable {
     constructor(PexfiToken asset_)
         ERC4626(asset_)
         ERC20("Staked Pexfi P2P", "sPEXFI")
         ERC20Permit("Staked Pexfi P2P")
         Ownable(msg.sender)
     {}
-
-    // The following functions are overrides required by Solidity.
-
-    function _update(address from, address to, uint256 value)
-        internal
-        override(ERC20, ERC20Votes)
-    {
-        super._update(from, to, value);
-    }
-
-    function nonces(address owner)
-        public
-        view
-        override(ERC20Permit, Nonces)
-        returns (uint256)
-    {
-        return super.nonces(owner);
-    }
 
     function decimals() public view override(ERC4626, ERC20) returns (uint8) {
         return super.decimals();
