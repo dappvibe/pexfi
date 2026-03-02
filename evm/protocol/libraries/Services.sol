188c261bb637c409a24250dba8f907dc8377dc28
Maksim <max@dappvibe.com>
2026-03-02 14:18:14 +0500

    rename FinderConstants to Services
    for clarity

---
+++ evm/protocol/libraries/Services.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.34;
+
+library Services {
+  bytes32 public constant Market = "Market";
+  bytes32 public constant OfferImplementation = "OfferImplementation";
+  bytes32 public constant DealImplementation = "DealImplementation";
+  bytes32 public constant Profile = "Profile";
+  bytes32 public constant Oracle = "Oracle";
+  bytes32 public constant Store = "Store";
+  bytes32 public constant IdentifierWhitelist = "IdentifierWhitelist";
+  bytes32 public constant CollateralWhitelist = "CollateralWhitelist";
+  bytes32 public constant FeeCollector = "FeeCollector";
+  bytes32 public constant Uniswap = "Uniswap";
+  bytes32 public constant PexfiVault = "PexfiVault";
+}
