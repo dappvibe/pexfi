118140a3dbe5cd5c202d4dce1ac38e0bc9b5d178
Maksim Tsepkov <max@dappvibe.com>
2026-03-02 16:00:59 +0500

    move event definition to interface
    for consistency

--- evm/protocol/interfaces/IFeeCollector.sol
+++ evm/protocol/interfaces/IFeeCollector.sol
@@ -2,5 +2,7 @@
 pragma solidity 0.8.34;
 
 interface IFeeCollector {
+  event Buyback(address indexed token, uint256 amountIn, uint256 amountPexfiOut);
+
   function buyback(address token, uint24 fee) external payable;
 }
