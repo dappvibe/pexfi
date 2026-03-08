9cf2b8b71f180b69db309ad6231a8d25cefe3d8a
Maksim <max@dappvibe.com>
2026-03-09 00:45:44 +0500

    fix bug with incorrect astronomical price
    uniswap tokens are sorted alphabetically so it is not guranteed stablecoin is the second, so math is adjusted to detect this

--- evm/protocol/mocks/MockUniswapV3Factory.sol
+++ evm/protocol/mocks/MockUniswapV3Factory.sol
@@ -2,6 +2,14 @@
 pragma solidity 0.8.34;
 
 contract PoolBTC {
+  address public token0;
+  address public token1;
+
+  constructor(address _token0, address _token1) {
+    token0 = _token0;
+    token1 = _token1;
+  }
+
   function observe(uint32[] calldata secondsAgos) external view
   returns (int56[] memory tickCumulatives, uint160[] memory secondsPerLiquidityCumulativeX128s)
   {
@@ -17,16 +25,24 @@ contract PoolBTC {
 }
 
 contract PoolETH {
+  address public token0;
+  address public token1;
+
+  constructor(address _token0, address _token1) {
+    token0 = _token0;
+    token1 = _token1;
+  }
+
   function observe(uint32[] calldata secondsAgos) external view
   returns (int56[] memory tickCumulatives, uint160[] memory secondsPerLiquidityCumulativeX128s)
   {
     // WETH / USDC pool
     int56[] memory tickCumulatives = new int56[](2);
-    tickCumulatives[0] = - 17595288323282;
-    tickCumulatives[1] = - 17595346759502;
+    tickCumulatives[0] = 12276842837088;
+    tickCumulatives[1] = 12276898814988;
     uint160[] memory secondsPerLiquidityCumulativeX128s = new uint160[](2);
-    secondsPerLiquidityCumulativeX128s[0] = 431388776603627029670683737059;
-    secondsPerLiquidityCumulativeX128s[1] = 431388776750562642834933065513;
+    secondsPerLiquidityCumulativeX128s[0] = 2213225794400229725653573790220191;
+    secondsPerLiquidityCumulativeX128s[1] = 2213225802448719423447484547581543;
     return (tickCumulatives, secondsPerLiquidityCumulativeX128s);
   }
 }
