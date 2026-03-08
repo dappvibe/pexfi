36fb8426baad471edb11871ca633e1fb8689fcca
Maksim Tsepkov <max@dappvibe.com>
2026-03-08 20:31:08 +0500

    deploy subgraph to graph studio
    in just works with draconic limits, but works. can go to decentralized indexers when got users

--- src/subgraph.config.ts
+++ src/subgraph.config.ts
@@ -2,6 +2,6 @@ import { mainnet, sepolia, hardhat } from 'wagmi/chains'
 
 export const SUBGRAPH_URLS: Record<number, string> = {
   [mainnet.id]: 'https://api.studio.thegraph.com/query/1741439/pexfi/version/latest',
-  [sepolia.id]: 'https://graph.pexfi.com/subgraphs/name/sepolia',
+  [sepolia.id]: 'https://api.studio.thegraph.com/query/1741439/sepolia/latest',
   [hardhat.id]: 'http://localhost:8000/subgraphs/name/hardhat',
 }
