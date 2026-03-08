c17c25e0d2f483c7ba835c5ebdff119308b577e6
Maksim Tsepkov <max@dappvibe.com>
2026-03-08 20:37:57 +0500

    correct url

--- src/subgraph.config.ts
+++ src/subgraph.config.ts
@@ -2,6 +2,6 @@ import { mainnet, sepolia, hardhat } from 'wagmi/chains'
 
 export const SUBGRAPH_URLS: Record<number, string> = {
   [mainnet.id]: 'https://api.studio.thegraph.com/query/1741439/pexfi/version/latest',
-  [sepolia.id]: 'https://api.studio.thegraph.com/query/1741439/sepolia/latest',
+  [sepolia.id]: 'https://api.studio.thegraph.com/query/1741439/sepolia/version/latest',
   [hardhat.id]: 'http://localhost:8000/subgraphs/name/hardhat',
 }
