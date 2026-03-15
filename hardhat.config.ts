70402876f79d317e632156d564b968256c97e4c9
Maksim <max@dappvibe.com>
2026-03-16 01:13:53 +0500

    ci/cd: deploy contracts with docker
    now fully ready env can be done in one `docker compose up all`

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -39,6 +39,11 @@ export default defineConfig({
       type: 'http',
       url: 'http://localhost:8545',
     },
+    compose: {
+      chainId: 31337,
+      type: 'http',
+      url: 'http://evm:8545',
+    },
     arbitrum: {
       type: 'http',
       url: process.env.ARBITRUM_URL ? process.env.ARBITRUM_URL : 'https://arb-mainnet.g.alchemy.com/v2/demo',
