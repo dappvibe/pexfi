c7bea3d0ab1fb02da5678d39555efb12fff36ce3
Maksim Tsepkov <max@dappvibe.com>
2026-03-15 23:48:51 +0500

    chore: create offers for deal tests directly with RPC

--- tests/e2e/deal/deal-flow.test.ts
+++ tests/e2e/deal/deal-flow.test.ts
@@ -1,15 +1,16 @@
 import { test, expect } from '@tests/e2e/setup'
 import { accept, fund, markPaid, release, leaveFeedback } from './actions'
+import { createOffer } from '../fixtures'
 
 test.describe.serial('Deal flow', () => {
   test('maker is buying', async ({ createParty }) => {
     // Create Offer
+    const offerAddress = await createOffer({}, 0)
     const maker = await createParty()
     await maker.setAccount(0)
-    const offer = await maker.createOffer()
 
     // Open Deal
-    const taker = await createParty(`/trade/offer/${offer.address}`)
+    const taker = await createParty(`/trade/offer/${offerAddress}`)
     await taker.setAccount(1)
     await taker.page.getByPlaceholder('Crypto Amount').fill('0.1')
     await taker.page.getByPlaceholder('Fiat Amount').fill('150')
@@ -41,12 +42,12 @@ test.describe.serial('Deal flow', () => {
 
   test('maker is selling', async ({ createParty }) => {
     // Create Sell Offer
+    const offerAddress = await createOffer({ isSell: true }, 0)
     const maker = await createParty()
     await maker.setAccount(0)
-    const offer = await maker.createOffer({ isSell: true })
 
     // Open Deal
-    const taker = await createParty(`/trade/offer/${offer.address}`)
+    const taker = await createParty(`/trade/offer/${offerAddress}`)
     await taker.setAccount(1)
     await taker.page.getByPlaceholder('Crypto Amount').fill('0.1')
     await taker.page.getByPlaceholder('Fiat Amount').fill('150')
