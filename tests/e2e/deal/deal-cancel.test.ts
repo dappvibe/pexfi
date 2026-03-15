22ca922918ad8b21fb15d49ec2d16a8d4dd2de10
Maksim <max@dappvibe.com>
2026-03-15 23:48:51 +0500

    chore: create offers for deal tests directly with RPC

--- tests/e2e/deal/deal-cancel.test.ts
+++ tests/e2e/deal/deal-cancel.test.ts
@@ -1,13 +1,14 @@
 import { test } from '@tests/e2e/setup'
 import { cancel, expectState } from './actions'
+import { createOffer } from '../fixtures'
 
 test.describe.serial('Deal cancellation', () => {
   test('maker cancels before accepting', async ({ createParty }) => {
+    const offerAddress = await createOffer({}, 0)
     const maker = await createParty()
     await maker.setAccount(0)
-    const offer = await maker.createOffer()
 
-    const taker = await createParty(`/trade/offer/${offer.address}`)
+    const taker = await createParty(`/trade/offer/${offerAddress}`)
     await taker.setAccount(1)
     await taker.page.getByPlaceholder('Crypto Amount').fill('0.1')
     await taker.page.getByPlaceholder('Fiat Amount').fill('150')
