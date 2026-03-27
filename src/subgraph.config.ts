180d4a47a663eb79b7063e9c55d65065bf399ffe
Maksim <max@dappvibe.com>
2026-03-27 21:24:43 +0500

    mainnet subgraph deployment endpoint

--- src/subgraph.config.ts
+++ src/subgraph.config.ts
@@ -1,7 +1,7 @@
 import { mainnet, sepolia, hardhat } from 'wagmi/chains'
 
 export const SUBGRAPH_URLS: Record<number, string> = {
-  [mainnet.id]: 'https://api.studio.thegraph.com/query/1741439/pexfi/version/latest',
+  [mainnet.id]: 'https://api.studio.thegraph.com/query/1745305/mainnet/version/latest',
   [sepolia.id]: 'https://api.studio.thegraph.com/query/1741439/sepolia/version/latest',
   [hardhat.id]: 'http://localhost:8000/subgraphs/name/hardhat',
 }
